ce16e8af66168b87c8f80a51390a1adbeee5851a
Cam <cammclain@skiff.com>
2024-08-07 01:06:07 -0500

    chore: Add ASGI file for Commander project

---
+++ src/asgi.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import asyncio
+
+from rich.console import Console
+from rich.prompt import Prompt
+from datetime import datetime
+
+
+
+
+
+
+async def main() -> None:
+
+    console: Console = Console()    
+
+    console.print(f"Initializing your commander environment", style="bold green")
+
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
+
+
+    
